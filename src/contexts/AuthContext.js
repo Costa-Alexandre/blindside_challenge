8fcd3b950d09c468ae40dac7ed5424b28e5df5d3
Alexandre Costa <alexandre.costa@code.berlin>
2022-05-27 19:19:54 +0200

    feat: sign in with google

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -7,10 +7,13 @@ import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   onAuthStateChanged,
+  signInWithPopup,
+  GoogleAuthProvider,
 } from 'firebase/auth';
 import { auth } from '../firebase';
 
 const AuthContext = React.createContext();
+const provider = new GoogleAuthProvider();
 
 export function useAuth() {
   return useContext(AuthContext);
@@ -26,6 +29,25 @@ export function AuthProvider({ children }) {
 
   function login(email, password) {
     return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  async function signInWithGoogle() {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+    } catch (error) {
+      console.log(error);
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+    }
   }
 
   function logout() {
@@ -61,6 +83,7 @@ export function AuthProvider({ children }) {
     login,
     currentUser,
     signup,
+    signInWithGoogle,
   };
 
   return (
