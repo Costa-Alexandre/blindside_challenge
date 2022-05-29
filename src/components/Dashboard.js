d5899be4502525c224a20146ad3d5caf891c927a
Alexandre Costa <alexandre.costa@code.berlin>
2022-05-29 02:24:06 +0200

    fix: clean up Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Button, Alert } from 'react-bootstrap';
+import { Alert } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { ref, getStorage, getDownloadURL } from 'firebase/storage';
 import ReactPlayer from 'react-player';
+import Navbar from './Navbar';
 
 export default function Dashboard() {
   const [error, setError] = useState('');
-  const { currentUser, logout } = useAuth();
+  const { logout } = useAuth();
   const navigate = useNavigate();
   const { message } = useLocation().state || '';
   const [url, setUrl] = useState('');
@@ -23,8 +24,6 @@ export default function Dashboard() {
     }
   }
 
-  const { displayName, email, photoURL } = currentUser;
-
   const getUrl = async (path) => {
     const storage = getStorage();
     const url = await getDownloadURL(ref(storage, path));
@@ -37,22 +36,13 @@ export default function Dashboard() {
 
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Profile</h2>
-          {error && <Alert variant="danger">{error} </Alert>}
-          {message && <Alert variant="success">{message} </Alert>}
-          <strong>Email: {email} </strong>
-          <p>Name: {displayName}</p>
-          <img src={photoURL} alt="profile pic" />
-          <ReactPlayer url={url} controls={true} />
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogout}>
-          Log out
-        </Button>
-      </div>
+      <Navbar />
+      {error && <Alert variant="danger">{error} </Alert>}
+      {message && <Alert variant="success">{message} </Alert>}
+      <ReactPlayer url={url} controls={true} />
+      <button variant="link" onClick={handleLogout}>
+        Log out
+      </button>
     </>
   );
 }
