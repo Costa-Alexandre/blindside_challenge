94c15fe84da8d7296afbc8843b2176e8a8adf22f
Alexandre Costa <alexandre.costa@code.berlin>
2022-05-29 02:23:54 +0200

    feat: create Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from '../assets/Logotype_White_Web.svg';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import '../styles/Navbar.css';
+import profilePlaceholder from '../assets/profile-placeholder.jpg';
+
+function Navbar() {
+  const { currentUser, logout } = useAuth();
+  const { displayName, email, photoURL } = currentUser;
+
+  return (
+    <navbar>
+      <div className="navbar-container">
+        <div className="navbar-brand">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="navbar-links">
+          <Link to="/library">Blindside Library</Link>
+        </div>
+        <div className="navbar-profile">
+          <img src={photoURL ? photoURL : profilePlaceholder} alt="profile" />
+        </div>
+      </div>
+    </navbar>
+  );
+}
+
+export default Navbar;
